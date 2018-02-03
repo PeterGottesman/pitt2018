f7afb65374bd407884c34e747757b0d4ebb5eb8c
Ryan Dils <dilsy3@gmail.comssh-keygen -t rsa -C dilsy3@gmail.comcat>
2018-02-03 03:22:28 -0500

    added reviews tables and edited reviewers table

--- backend/database/db.sql
+++ backend/database/db.sql
@@ -1,10 +1,19 @@
-CREATE DATABASE doctrate;
-
 CREATE TABLE reviewers
 (
-personID INT,
+reviewerID SERIAL PRIMARY KEY,
 lastName VARCHAR (255),
 firstName VARCHAR (255),
 specialty VARCHAR (255),
 experience INT,
+authentication TINYINT (1)
+);
+
+CREATE TABLE reviews
+(
+ratingID SERIAL,
+url VARCHAR (2083),
+title VARCHAR (255),
+author VARCHAR (255),
+rating TINYINT,
+reviews BIGINT
 );
