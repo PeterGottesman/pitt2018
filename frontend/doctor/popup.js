73540807921ef41e74da2047bd653969ce0d626d
Peter Gottesman <peter@petergottesman.com>
2018-02-03 10:04:50 -0500

    Fixed inline script error, frontend works correctly

--- frontend/doctor/popup.js
+++ frontend/doctor/popup.js
@@ -68,11 +68,7 @@ function setVals(url, title, author) {
     }
 }
 
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
+window.addEventListener('load', function load(event){
+    // var createButton = document.getElementById('create_button');
+    getCurrentTabUrl(setVals);
+});
